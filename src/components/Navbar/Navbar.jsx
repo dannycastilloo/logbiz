44a8a5cef44d91d021c7d2db974fa072f1ae0f41
dannycastilloo <dannycastillootiniano@gmail.com>
2024-04-21 22:50:47 -0400

    last navbar details

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,8 @@
-import { NavLink } from "react-router-dom";
-import { useState } from "react";
-import { NavbarLink } from "./NavbarLink";
-import { Sidebar } from "./Sidebar";
+import { NavLink } from "react-router-dom"
+import { useState } from "react"
+import { NavbarLink } from "./NavbarLink"
+import { Sidebar } from "./Sidebar"
+import { NavbarButton } from "./NavbarButton"
 
 export const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false)
@@ -12,10 +13,25 @@ export const Navbar = () => {
 
     return (
         <header className='flex justify-between items-center w-11/12 rounded-md fixed bg-black px-6 md:px-8 py-4'>
+
+            {/* Navigation Links (Hidden on Small Devices) */}
+            <nav className={`hidden lg:flex justify-start items-center gap-2 w-4/12 ${menuOpen ? 'hidden' : ''}`}>
+                <NavbarLink to='/home' view='Home'></NavbarLink>
+                <NavbarLink to='/services' view='Services'></NavbarLink>
+                <NavbarLink to='/dashboard' view='Dashboard'></NavbarLink>
+                <NavbarLink to='/services-listing' view='Service Listing'></NavbarLink>
+            </nav>
+
             {/* Logo */}
             <NavLink to='/home' className="w-1/4 md:w-1/2 lg:w-4/12 flex justify-start items-center lg:justify-center">
                 <img className="w-1/2 md:w-2/6 lg:w-3/12" src="./logo.png" alt="Logbiz Logo" title='Logbiz Logo' />
             </NavLink>
+
+            {/* Buttons */}
+            <nav className='hidden lg:flex justify-end items-center gap-2 w-4/12'>
+                <NavbarButton to='/contact' view='Contact us'></NavbarButton>
+                <NavbarButton to='/login' view='Sign up/login'></NavbarButton>
+            </nav>
 
             {/* Hamburger Menu Button */}
             <button className="lg:hidden" onClick={toggleMenu}>
@@ -26,14 +42,6 @@ export const Navbar = () => {
 
             {/* Sidebar */}
             <Sidebar isOpen={menuOpen} onClose={toggleMenu} />
-
-            {/* Navigation Links (Hidden on Small Devices) */}
-            <nav className={`hidden lg:flex justify-start items-center gap-2 w-4/12 ${menuOpen ? 'hidden' : ''}`}>
-                <NavbarLink to='/home' view='Home'></NavbarLink>
-                <NavbarLink to='/services' view='Services'></NavbarLink>
-                <NavbarLink to='/dashboard' view='Dashboard'></NavbarLink>
-                <NavbarLink to='/services-listing' view='Service Listing'></NavbarLink>
-            </nav>
         </header>
     )
 }
